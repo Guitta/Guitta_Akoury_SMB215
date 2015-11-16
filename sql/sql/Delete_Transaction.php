83103fc6ef215cffc99067759ecb9218e7f895ec
Guitta <akouryguitta@hotmail.com>
2015-11-16 18:43:45 +0200

    Delete Delete_Transaction.php

--- sql/sql/Delete_Transaction.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '$config.php';
-$transaction_id = $_REQUEST['transaction_id'];
-
-mysql_query("delete from `transaction` where transaction_id = '$transaction_id'",$con);
-mysql_close($con);
-?> 
